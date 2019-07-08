62554583c4ff1b6f8fab10732d0877d521d08a10
Benjamin Bilbeau <benjamin.bilbeau@avanade.com>
2019-07-08 17:03:40 -0400

    Fixed styling for transaction side

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,10 +3,14 @@ import React, {Component} from "react";
 class Transaction extends Component{
 
     render() {
+
+        var sideClassName = "stock-transactions__grid-text";
+        sideClassName += this.props.side.toUpperCase() == 'SELL' ? " stock-transactions__grid-cell-sell" : " stock-transactions__grid-cell-buy";
+
         return <div className="stock-transactions__grid-row">
             <div className="stock-transactions__grid-cell"><span className="stock-transactions__grid-text">{this.props.symbol}</span></div>
             <div className="stock-transactions__grid-cell"><span className="stock-transactions__grid-text">{this.props.amount}</span></div>
-            <div className="stock-transactions__grid-cell"><span className="stock-transactions__grid-text stock-transactions__grid-cell-sell">{this.props.side}</span></div>
+            <div className="stock-transactions__grid-cell"><span className={sideClassName}>{this.props.side}</span></div>
             <div className="stock-transactions__grid-cell"><span className="stock-transactions__grid-text">{this.props.tickPrice}</span></div>
             <div className="stock-transactions__grid-cell"><span className="stock-transactions__grid-text">{this.props.cost}</span></div>
         </div>;
